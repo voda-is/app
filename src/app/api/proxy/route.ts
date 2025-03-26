7987e3511687617217ca3ec340942f84a5213fa3
RoyTimes <songz@duck.com>
2025-03-26 15:00:39 -0700

    change name for conn to backend server

--- src/app/api/proxy/route.ts
+++ src/app/api/proxy/route.ts
@@ -1,7 +1,7 @@
 import { createAuthToken } from "@/lib/authToken";
 import { NextRequest, NextResponse } from "next/server";
 
-const API_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+const API_URL = process.env.API_BASE_URL;
 export async function POST(request: NextRequest) {
   try {
     const body = await request.json();
